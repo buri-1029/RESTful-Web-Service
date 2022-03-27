7f00137d7724119bd7cc01998c0f9e02e5156cb9
buri-1029 <kimbuhui@naver.com>
2022-03-27 23:59:12 +0900

    Update Dto Class 변경
    - UpdateUserRequest Validation 추가
    - UserResponse 데이터 제어 Filtering 추가
      - @JsonIgnore

--- src/main/java/com/buri/restfulservice/domain/user/dto/UpdateUserRequest.java
+++ src/main/java/com/buri/restfulservice/domain/user/dto/UpdateUserRequest.java
@@ -1,5 +1,6 @@
 package com.buri.restfulservice.domain.user.dto;
 
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UpdateUserRequest {
 
+	@Size(min = 2, message = "이름은 2글자 이상 입력해주세요.")
 	private String name;
+
+	@Size(min = 8, message = "비밀번호는 8글자 이상 입력해주세요.")
 	private String password;
 }
